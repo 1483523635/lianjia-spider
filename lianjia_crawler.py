002378a3f391d45583ad9e2ee7f59801eb2d501a
XiaoQu <lqqu@thoughtworks.com>
2021-10-06 12:46:27 +0800

    [XiaoQu] #N/A feat: 甜水园每个小区的均价

--- lianjia_crawler.py
+++ lianjia_crawler.py
@@ -16,22 +16,22 @@ import json
 
 import threading
 
-
 import time
+
+import os
 
 
 def get_list_page_url(url):
-
     start_url = url
     init_url = start_url.format("")
-    headers =  {
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
     }
     try:
         response = requests.get(start_url.format(""), headers=headers)
         # print(response.status_code, response.text)
         doc = pq(response.text)
-        total_num =  int(doc(".resultDes .total span").text())
+        total_num = int(doc(".resultDes .total span").text())
         total_page = total_num // 30 + 1
         # 只能访问到前一百页
         if total_page > 100:
@@ -41,7 +41,7 @@ def get_list_page_url(url):
 
         for i in range(total_page):
             page_url_list.append(start_url.format("pg" + str(i + 1)))
-            #print(url)
+            # print(url)
         return page_url_list
 
     except:
@@ -50,6 +50,7 @@ def get_list_page_url(url):
 
 
 detail_list = list()
+
 
 # 需要先在本地开启代理池
 # 代理池仓库: https://github.com/Python3WebSpider/ProxyPool
@@ -64,13 +65,13 @@ def get_valid_ip():
 
 def get_detail_page_url(page_url):
     global detail_list
-    headers =  {
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
         'Referer': 'https://bj.lianjia.com/ershoufang'
     }
 
     try:
-        response = requests.get(page_url,headers=headers,timeout=3)
+        response = requests.get(page_url, headers=headers, timeout=3)
         doc = pq(response.text)
         # broswer.get(page_url)
         # print(page_url)
@@ -90,7 +91,62 @@ def get_detail_page_url(page_url):
     except:
         print("获取列表页" + page_url + "出错")
 
+
 lock = threading.Lock()
+
+
+def send_request(url, with_proxy=False):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        'Referer': 'https://bj.lianjia.com/ershoufang'
+    }
+
+    if not with_proxy:
+        return requests.get(url=url, headers=headers, timeout=3)
+    proxies = {
+        "http": "http://" + get_valid_ip(),
+    }
+    return requests.get(url=url, headers=headers, proxies=proxies, timeout=3)
+
+
+def extract_response(response, detail_url):
+    detail_dict = dict()
+    doc = pq(response.text)
+    unit_price = doc(".unitPriceValue").text()
+    unit_price = unit_price[0:unit_price.index("元")]
+    title = doc("h1").text()
+    area = doc(".areaName .info a").eq(1).text().strip()
+    total_price = doc(".total").text()
+    xiaoqu_name = doc(".communityName .info").text()
+    lou_ceng = doc(".subInfo").eq(0).text()
+    zhuang_kuang = doc(".subInfo").eq(1).text()
+    fangzi_detail = doc(".subInfo").eq(2).text()
+    fang_ling = fangzi_detail[0: fangzi_detail.index("\n")]
+    # 塔楼， 板楼
+    lou_kuang = fangzi_detail[fangzi_detail.index("\n/")+2:len(fangzi_detail)]
+
+    hu_xing = doc(".mainInfo").eq(0).text()
+    chao_xiang = doc(".mainInfo").eq(1).text()
+    mian_ji = doc(".mainInfo").eq(2).text()
+
+    url = detail_url
+    detail_dict["title"] = title
+    detail_dict["area"] = area
+    detail_dict["price"] = unit_price
+    detail_dict["url"] = url
+    detail_dict["total_price"] = total_price
+    detail_dict["xiaoqu_name"] = xiaoqu_name
+    detail_dict["lou_ceng"] = lou_ceng
+    detail_dict["zhuang_kuang"] = zhuang_kuang
+    detail_dict["fang_ling"] = fang_ling
+    detail_dict["lou_kuang"] = lou_kuang
+    detail_dict["hu_xing"] = hu_xing
+    detail_dict["chao_xiang"] = chao_xiang
+    detail_dict["mian_ji"] = mian_ji
+
+    detail_list.append(detail_dict)
+    print(unit_price, title, area, total_price, xiaoqu_name)
+
 
 def detail_page_parser(res):
     global detail_list
@@ -98,70 +154,38 @@ def detail_page_parser(res):
     if not detail_urls:
         print("detail url 为空")
         return None
-    headers =  {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-        'Referer': 'https://bj.lianjia.com/ershoufang'
-    }
+
     for detail_url in detail_urls:
         try:
-            response = requests.get(url=detail_url, headers=headers,timeout=3)
-            #print(response.status_code)
-            detail_dict = dict()
-            doc = pq(response.text)
-            unit_price = doc(".unitPriceValue").text()
-            unit_price = unit_price[0:unit_price.index("元")]
-            title = doc("h1").text()
-            area = doc(".areaName .info a").eq(1).text().strip()
-            url = detail_url
-            detail_dict["title"] = title
-            detail_dict["area"] = area
-            detail_dict["price"] = unit_price
-            detail_dict["url"] = url
-
-            detail_list.append(detail_dict)
-
-            print(unit_price, title, area)
+            response = send_request(detail_url)
+            extract_response(response, detail_url)
 
         except:
             print("获取详情页出错,换ip重试")
-            proxies = {
-                "http": "http://" + get_valid_ip(),
-            }
+            beep()
             try:
-                response = requests.get(url=detail_url, headers=headers, proxies=proxies)
-                #print(response.status_code)
-                detail_dict = dict()
-                doc = pq(response.text)
-                unit_price = doc(".unitPriceValue").text()
-                unit_price = unit_price[0:unit_price.index("元")]
-                title = doc("h1").text()
-                area = doc(".areaName .info a").eq(0).text().strip()
-                url = detail_url
-                # 已下架的还会爬取，但是没有价格
-                if len(unit_price)>0:
-                    detail_dict["title"] = title
-                    detail_dict["area"] = area
-                    detail_dict["price"] = unit_price
-                    detail_dict["url"] = url
-
-                    detail_list.append(detail_dict)
-
-                    print(unit_price, title, area)
+                response = send_request(detail_url, True)
+                extract_response(response, detail_url)
             except:
-                print("重试失败...")
+                beep()
                 time.sleep(20)
 
 
-def save_data(data,filename):
-    with open(filename+".json", 'w', encoding="utf-8") as f:
+def beep():
+    os.system('say "error happened."')
+
+
+def save_data(data, filename):
+    with open(filename + ".json", 'w', encoding="utf-8") as f:
         f.write(json.dumps(data, indent=2, ensure_ascii=False))
 
+
 def load_data_from_file():
-    data=[]
-    with open("url.json","r",encoding="utf-8") as f:
+    with open("url.json", "r", encoding="utf-8") as f:
         str = f.read()
         data = json.loads(str)
     return data
+
 
 def main():
     # cq,cs,nj,dl,wh,cc
@@ -184,21 +208,14 @@ def main():
 
         print(detail_list)
 
-    save_data(detail_list, "chaoyang-new")
+    save_data(detail_list, "chaoyang-tianshuiyuan-1-2-ju-banlou")
 
     detail_list.clear()
+
 
 if __name__ == '__main__':
     old = time.time()
     main()
-    new  = time.time()
+    new = time.time()
     delta_time = new - old
     print("程序共运行{}s".format(delta_time))
-
-
-
-
-
-
-
-
--- test.py
+++ test.py
@@ -1,23 +1,32 @@
-import json 
-import AppKit
-AppKit.NSBeep()
+import json
+import sys
+import os
+
+
 
 def load_data_from_file():
-    data=[]
-    with open("url.json","r",encoding="utf-8") as f:
+    data = []
+    with open("url.json", "r", encoding="utf-8") as f:
         str = f.read()
         data = json.loads(str)
     # print(data)
     return data
 
+
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
+
 def main():
-    data =  load_data_from_file()
+    data = load_data_from_file()
     for url in data:
         print(url.format(""))
 
 
+def beep():
+    os.system('say "error happened."')
+
+
 if __name__ == '__main__':
-    main()
+    beep()
+    # main()
